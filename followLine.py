6990b253f7e7f0b5bf4470e83e8a5ee71cfc439d
scott-ritchie-1394 <cheasescott@gmail.com>
2016-10-18 18:44:19 +0000

    Implemented threading

--- followLine.py
+++ followLine.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import sys
 sys.path.append('/usr/local/lib/python2.7/site-packages')
 from picamera.array import PiRGBArray
+from PiVideoStream import PiVideoStream
 from picamera import PiCamera
 import numpy as np
 import time
+import imutils
 import cv2
 import io
 
+
+width = 640
+height = 480
 Kp = 2
 Kd = 1
 dt = 0
-horLine = 200
+horLine = 380
 colors = [[0,0,0],[255,0,0],[0,255,0],[0,0,255],[255,255,0],[255,0,255],[0,255,255],[255,255,255],[0,0,0]]
 def convertToBits(number):
 	bits = []
@@ -22,18 +28,13 @@ def convertToBits(number):
 		else:
 			bits.append(1)
 	return bits
-def findCenter(camera,stream):
-	start = time.time()
-	#stream = io.BytesIO()
-	camera.capture(stream, format='bgr',use_video_port=True)
-	data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-	print("Capture time: " + str(time.time() - start))
-	imgc = stream.array
+def findCenter(vs):
+	imgc = vs.read()
 	img = cv2.cvtColor(imgc,cv2.COLOR_BGR2GRAY)
 	thresh1 = cv2.threshold(img,150,255,cv2.THRESH_BINARY)
 	count = 0
 	sum = 0
-	for y in range(0,320):
+	for y in range(0,width):
 		if thresh1[1][horLine][y] == 255:
 			count += 1
 			sum += y
@@ -42,19 +43,18 @@ def findCenter(camera,stream):
 		avg = int(sum / count)
 	else:
 		avg = -1
-	PD(avg - 160)
+	PD(avg - (width/2))
 def PD(error):
 	Pvalue = Kp*error
 	Dvalue = Kd * (error - dt)
 	PD = Pvalue + Dvalue
 	return PD 
-with PiCamera() as camera:
-	with PiRGBArray(camera) as stream:
-		camera.resolution = (320,240)
-		myCount = 0
-		while myCount < 100:
-			startTotal = time.time()
-			findCenter(camera,stream)
-			stream.truncate(0)
-			print("Time = " + str(time.time() - startTotal))
-			myCount += 1 
+vs = PiVideoStream().start()
+time.sleep(2.0)
+myCount = 0
+startLoop = time.time()
+while myCount < 100:
+	findCenter(vs)
+	myCount += 1
+print(str(100/(time.time() - startLoop)) + " FPS")
+vs.stop() 
